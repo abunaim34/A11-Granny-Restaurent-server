2439d944112340999ea8beb6e111dab3ce09fb4a
abunaim34 <farhanadnanfarabi@gmail.com>
2024-05-11 23:52:19 +0600

    get Data from database

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,12 +10,68 @@ const corsOptions = {
     origin: [
         'http://localhost:5173',
         'http://localhost:5174',
+        'https://granny-resturant.web.app',
+        'https://granny-resturant.firebaseapp.com',
     ],
     credentials: true,
     optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyfftle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+        const foodsCollection = client.db('FoodsDB').collection('foods')
+
+        app.get('/foods', async (req, res) => {
+            const cursor = foodsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        // app.get('/food/:email', async(req, res) => {
+        //     // const email = {email: req.params.email}
+        //     const cursor = foodsCollection.find({ email: req.params.email })
+        //     const result = await cursor.toArray()
+        //     res.send(result)
+        // })
+        app.get('/foods/:email', async (req, res) => {
+            const cursor = foodsCollection.find({ email: req.params.email })
+            const result = await cursor.toArray()
+            res.send(result)
+          })
+
+        app.post('/foods', async(req, res) => {
+            const addFoods = req. body
+            const result = await foodsCollection.insertOne(addFoods)
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('GRANNY RESTURANT')
